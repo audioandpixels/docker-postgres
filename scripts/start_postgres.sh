71ef8e84f92d38dd2c7e58c3e151be6a6a7535f8
jasoncox <jason@audioandpixels.com>
2014-12-19 11:04:06 +1100

    Change password with unix domain socket

--- scripts/start_postgres.sh
+++ scripts/start_postgres.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/ensure_ownership.sh
 exec su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf"
-exec su postgres -c " psql -h 0.0.0.0 -c "ALTER USER postgres WITH PASSWORD '$PASSWORD';""
+exec su postgres -c "psql -c "ALTER USER postgres WITH PASSWORD '$PASSWORD';""
