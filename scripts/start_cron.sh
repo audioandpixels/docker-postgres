48b9ac8a8210b2d52d8f97010dd4004afc4ddf8c
jasoncox <jason@audioandpixels.com>
2014-12-21 09:16:21 +1100

    Envdir for cron

--- scripts/start_cron.sh
+++ scripts/start_cron.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/ensure_ownership.sh
+
+echo "$AWS_SECRET_ACCESS_KEY" > /etc/wal-e.d/env/AWS_SECRET_ACCESS_KEY
+echo "$AWS_ACCESS_KEY_ID" > /etc/wal-e.d/env/AWS_ACCESS_KEY_ID
+echo "$WALE_S3_PREFIX" > /etc/wal-e.d/env/WALE_S3_PREFIX
+
 exec cron -f
